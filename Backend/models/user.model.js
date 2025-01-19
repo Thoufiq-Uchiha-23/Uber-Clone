d5c3a41bb9823400ff4b85f54a8155115e9a3e7a
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-20 01:17:23 +0530

    auth

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   firstname: {
@@ -16,7 +18,25 @@ const userSchema = new mongoose.Schema({
     unique: true,
     minlength: [5, "Email must be atleast 5 characters"],
   },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
   socketId: {
     type: String,
   },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (password) {
+    return await  bcrypt.compare(password, this.password)
+}
+
+userSchema.statics.hashPassword = async function () {
+    return await bcrypt.hash(password, 10)
+}
